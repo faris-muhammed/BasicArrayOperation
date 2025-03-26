fd5c94b44f1851f79890c2d608f6130d746a18d2
Muhammed Faris <farismuhammed707@gmail.com>
2025-03-26 16:04:16 +0530

    Move zeroes to the right

---
+++ intermediateProblems/moveZeroes/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func moveZeroes(nums []int) {
+	j := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		}
+	}
+}
+
+func main() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+	fmt.Println("After moving zeroes:", nums)
+}
